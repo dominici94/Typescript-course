c3384adad34d2b26e9bd5a8c5bf1858e99d0bf72
dominici94 <mattiadominici94@gmail.com>
2023-07-14 20:02:40 +0200

    decorator factories

--- 04_decorators/src/app.ts
+++ 04_decorators/src/app.ts
@@ -4,13 +4,29 @@
 // }
 
 function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
 
-@Logger("LOGGING - PERSON")
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function (constructor: any) {
+    console.log("Rendering template");
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@Logger("Logging")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
@@ -22,3 +38,37 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ----------------
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("No negative values.");
+    }
+  }
+
+  // get price() {
+  //   return this._price;
+  // }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
